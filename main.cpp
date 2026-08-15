c783b29d4efa4cc7c27fa1a84941fb0a7df7f33e
Jovial Ninjio <jovialninjio@gmail.com>
2026-08-15 14:44:33 +0200

    Add initial C++ document reader

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(){
+  std::ifstream file("sample-data/example-document.txt");
+
+  if(!file.is_open()){
+    std::cout << "Could not open the document." << std::end;
+    return 1;
+  }
+
+  std::string line;
+
+  while(std::getline(file, line)){
+    std::cout<< line << std::endl;
+  }
+
+  file.close();
+
+  return 0;
+}
